d4a84cc6abacf51d2288bd00b77fe436083c4db2
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2022-10-23 01:30:46 +0300

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s "$1" -X DELETE
+# Displays the body of the response of a curl DELETE request
+curl -sLX DELETE "$1"
