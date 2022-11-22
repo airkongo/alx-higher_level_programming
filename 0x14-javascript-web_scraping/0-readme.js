18d4111f1e651f607cf63f9b4bc1f195d2faa833
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2022-11-22 12:49:34 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file
 
-const file = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(file, 'utf8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
 });
